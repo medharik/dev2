67034312410113122b68b9312dc4c2e00302f724
medharik <med.harik@gmail.com>
2024-10-23 15:44:00 +0100

    langage JS -exercice

---
+++ jsdev/s1/ex-mental1.js
@@ -0,0 +1,20 @@
+// ex1
+let a =[1,2];
+let x="0";
+let y=0;
+let z=0.0;
+
+for (let i = 0; i < a.length; i++) {
+    x+=a[i];
+    y+=a[i];
+    z+=a[i];   
+}
+console.log(x,y,z);
+//ex2 :
+let c=10;
+while(c-- > 7){
+    console.log('A',c);
+}
+while(--c > 2){
+    console.log('B',c);
+}
