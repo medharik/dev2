32ae04845fce55d193682ad50c4a3e46752c39df
medharik <med.harik@gmail.com>
2024-10-23 15:50:53 +0100

    tp 1  js

--- jsdev/s1/ex-mental1.js
+++ jsdev/s1/ex-mental1.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // ex1
-=======
->>>>>>> b4de68dd8249f4263fefd76f5d00748fc36c0374
 let a =[1,2];
 let x="0";
 let y=0;
@@ -10,7 +7,6 @@ let z=0.0;
 for (let i = 0; i < a.length; i++) {
     x+=a[i];
     y+=a[i];
-<<<<<<< HEAD
     z+=a[i];   
 }
 console.log(x,y,z);
@@ -22,9 +18,3 @@ while(c-- > 7){
 while(--c > 2){
     console.log('B',c);
 }
-=======
-    z+=a[i];
-    
-}
-console.log(x,y,z);
->>>>>>> b4de68dd8249f4263fefd76f5d00748fc36c0374
