ae97354fc7f740fe20870bd22d29d4003d0cadce
medharik <med.harik@gmail.com>
2024-10-23 16:13:36 +0100

    correction

--- jsdev/s1/ex-mental1.js
+++ jsdev/s1/ex-mental1.js
@@ -10,11 +10,16 @@ for (let i = 0; i < a.length; i++) {
     z+=a[i];   
 }
 console.log(x,y,z);
+//bonne reponse   : "012",3,3
+
 //ex2 :
 let c=10;
 while(c-- > 7){
-    colog('A',c);
+    console.log('A',c);
 }
+// REP 1 : A 9, A 8,A 7
+//APRES LA SORTIE DE LA BOUCLE c=6
 while(--c > 2){
     console.log('B',c);
 }
+// B 5, B 4 , B 3  
